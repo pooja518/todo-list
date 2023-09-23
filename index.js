54a7c320252490d469fae11854abb1a1f25667eb
pooja518 <poojacherupally518@gmail.com>
2023-09-23 22:16:18 +0530

    add express

---
+++ index.js
@@ -0,0 +1,13 @@
+const {request} = require('express');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=> {
+    res.send("Hello world!");
+});
+
+app.listen(port,()=>{
+    console.log(`Server started at node ${port}`);
+});
