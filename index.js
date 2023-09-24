ae3c85b6d6b1816093dbce3dd4b6f79395b014e5
pooja518 <poojacherupally518@gmail.com>
2023-09-24 21:08:57 +0530

    Added and marked as completed

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const { request } = require("express");
 
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const {Todo} = require("./models");
+
+
 
 app.get("/todos", (req, res) => {
-  //res.send("Hello world!");
+  res.send("Hello world!");
   console.log("Todo list");
+  
 });
 
-app.post("/todos", (req, res) => {
+app.post("/todos", async (req, res) => {
   console.log("Creating a todo", req.body);
+  try{
+    const todo = await Todo.addTodo({title: req.body.title, dueDate: req.body.dueDate, completed: false});
+    return res.json(todo);
+  }
+  catch(error){
+    console.log(error);
+    return res.status(422);
+  }
+  
 });
 
-app.put("/todos/:id/markAsCompleted",(req,res)=>{
+app.put("/todos/:id/markAsCompleted",async (req,res)=>{
     console.log("Update a todo with id :",req.params.id);
+    const todo = await Todo.findByPk(req.params.id);
+    try{
+      const updatedTodo = await todo.markAsCompleted();
+      return res.json(updatedTodo);
+    }
+    catch(error){
+      console.log(error);
+      return res.status(422);
+    }
+    
 });
 
 app.delete("/todos/:id",(req,res)=>{
-    console.log("delete a todo with id:",req.params.id);
+    console.log("delete a todo with id:",req.params.id); 
 });
 
 app.listen(port, () => {
