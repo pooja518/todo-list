ad3648ab7ba79e0e69bcc45880ea3e20c938e2e6
pooja518 <poojacherupally518@gmail.com>
2023-09-25 20:38:18 +0530

    index.js update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = require("./app");
 
 const port = 3000;
 
+app.get("/",(req,res)=> {
+  res.send(<h1>Hello world!</h1>);
+});
+
 app.listen(port, () => {
   console.log(`Server started at node ${port}`);
 });
