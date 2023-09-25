ea5c8263fe127598a89695a524af90c75e97be74
pooja518 <poojacherupally518@gmail.com>
2023-09-25 21:25:43 +0530

    Remove unwanted code from index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const app = require("./app");
 
 const port = 3000;
 
-app.get("/",(req,res)=> {
-  res.send(<h1>Hello world!</h1>);
-});
 
 app.listen(port, () => {
   console.log(`Server started at node ${port}`);
